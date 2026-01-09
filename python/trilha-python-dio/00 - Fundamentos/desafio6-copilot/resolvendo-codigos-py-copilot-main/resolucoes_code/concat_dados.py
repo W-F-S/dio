63753d97e121d1e36752c1afc509898c7a4e051c
W-F-S <walkerfreitassantos@gmail.com>
2026-01-09 17:12:56 -0300

    .

---
+++ python/trilha-python-dio/00 - Fundamentos/desafio6-copilot/resolvendo-codigos-py-copilot-main/resolucoes_code/concat_dados.py
@@ -0,0 +1,6 @@
+dado1 = input("primeiro dado: ")
+dado2 = input("segundo dado: ")
+
+resultado = dado1 + " " + dado2
+
+print("Resultado:", resultado)
---
+++ python/trilha-python-dio/00 - Fundamentos/desafio6-copilot/resolvendo-codigos-py-copilot-main/resolucoes_code/repet_txt.py
@@ -0,0 +1,5 @@
+texto = input("Digite o texto: ")
+vezes = int(input("Digite a quantidade de repetições: "))
+
+
+print((texto + " ") * vezes)
